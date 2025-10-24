a614c145befaac17a3a68da5707546376600b161
blockmonster1948-sketch <blockmonster1948@gmail.com>
2025-10-24 13:58:57 -0400

    Create HelloWorld.java

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/HelloWorld.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.robotcontroller;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Autonomous
+@Disabled
+public class HelloWorld extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "Marty");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
